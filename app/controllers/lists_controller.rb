9ef2c493add6460c3d5471ccd1122c2cf5580950
arnaud thiebart <arno.thieb@gmail.com>
2022-11-18 15:12:26 +0100

    exo2

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -6,4 +6,19 @@ class ListsController < ApplicationController
   def index
     @lists = List.all
   end
+
+  def create
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to list_path(@list)
+    else
+      render :new, status: :unprocessabke_entity
+    end
+  end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:name, :photo)
+  end
 end
